ea917d0dd92cad2042186f9a80fcfc0fd7d6de87
ElhamFayzi <fayze24@wfu.edu>
2026-06-23 17:21:37 -0400

    implement event recommendation functionality in genai_API.py

--- genai_API.py
+++ genai_API.py
@@ -18,3 +18,38 @@ def get_events_from_db():
         rows = connection.execute(db.text("SELECT event_id, name, url, date, venue FROM eventcache"))
     return rows
 
+def build_prompt(events, user_interests):
+    texts = ""
+    for i, event in enumerate(events, 1):
+        event_id, name, url, date, venue = event
+        texts += f"""
+                Event {i}:
+                - Name: {name}
+                - date: {date}
+                - venue: {venue}
+                - URL: {url}
+                """
+    
+    prompt = f"""You are an event recommendation assistant. The user is interested in: {user_interests}. Below is a list of upcoming events. Please: 
+                1. rank the events from most to least relevant based on the user's interests. 
+                2. for each event give a short 1 sentence explanation of why you recommended it
+                3. Please format your response cleanly using a numbered list.
+
+                Events: {texts}
+               """
+    return prompt
+
+def get_recommendation():
+    events = get_events_from_db()
+    user_interests = keyword
+
+    prompt = build_prompt(events, user_interests)
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+    return response.text
+
+
+
+print(get_recommendation())
