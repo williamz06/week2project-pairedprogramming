c3c82575db9a55be2bd74a3f9e76f1c405e38c38
williamz06 <william.z@wustl.edu>
2026-06-24 11:53:54 -0500

    added user input for user_interests in main

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from genai_API import get_recommendation
 def main():
     city = input('Enter city: ')
     keyword = input('Enter keyword (or press Enter to skip): ')
+    user_interests = input('Enter your interests: ')
     events = find_events(city, keyword)
     store_events(events)
-    recommendation = get_recommendation()
+
+    recommendation = get_recommendation(user_interests)
     print(recommendation)
 
 if __name__ == "__main__":
