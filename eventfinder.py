f0f1d318059e821e2eda9b6c45150c8529e159e2
Nicole Jallim <ncj2@fordham.edu>
2026-06-24 13:57:24 -0400

    Edited eventfinder.py to use find_events function and return event list.

--- eventfinder.py
+++ eventfinder.py
@@ -7,28 +7,48 @@ API_KEY = os.environ.get('TICKETMASTER_API_KEY')
 
 URL = 'https://app.ticketmaster.com/discovery/v2/events.json'
 
-city = input('Enter city: ')
-keyword = input('Enter keyword (or press Enter to skip): ')
-
-params = {
-   'apikey': API_KEY,
-   'city': city,
-   'keyword': keyword,
-   'sort': 'date,asc',
-   'size': 20 # <--  number of results, we can up the number if wanted
-}
 
 
-response = requests.get(URL, params=params)
-data = response.json()
+def find_events(city, keyword):
+   params = {
+      'apikey': API_KEY,
+      'city': city,
+      'keyword': keyword,
+      'sort': 'date,asc',
+      'size': 20 # <--  number of results, we can up the number if wanted
+   }
 
-events = data['_embedded']['events']
 
-print(f'\n20 Upcoming Events in {city}:')
-print('-' * 40)
+   response = requests.get(URL, params=params)
+   data = response.json()
 
-for event in events:
-   name = event['name']
-   date = event['dates']['start']['localDate']
-   venue = event['_embedded']['venues'][0]['name']
-   url = event['url']
+   events = data['_embedded']['events']
+
+   event_list = []
+
+   print(f'\n20 Upcoming Events in {city}:')
+   print('-' * 40)
+
+   for event in events:
+      name = event['name']
+      date = event['dates']['start']['localDate']
+      venue = event['_embedded']['venues'][0]['name']
+      url = event['url']
+      event_id = event['id']
+      city_name = event['_embedded']['venues'][0]['city']['name']
+
+      print(f'Event: {name}')
+      print(f'Date: {date}')
+      print(f'Venue: {venue}')
+      print(f'Tickets: {url}')
+      print('-' * 40)
+
+      event_list.append({
+         'id': event_id,
+         'name': name,
+         'url' : url,
+         'date' : date,
+         'venue': venue,
+         'city': city_name
+      })
+   return event_list
